cb4b5a3834c2886178d6e8008abd2fa717e1b539
Naga Hota <hotasmail@gmail.com>
2018-03-20 00:02:08 -0400

    anagram check

---
+++ anagram2.py
@@ -0,0 +1,26 @@
+def anagram1(str1, str2):
+    str1 = str1.replace(' ', '').lower()
+    str2 = str2.replace(' ', '').lower()
+
+    if len(str1) != len(str2):
+        return False
+
+    count = {}
+
+    for letter in str1:
+        if letter in count:
+            count[letter] += 1
+        else:
+            count[letter] = 1
+
+    for letter in str2:
+        if letter in count:
+            count[letter] -= 1
+        else:
+            count[letter] = 1
+
+    for k in count:
+        if count[k] != 0
+            return False
+
+    return True
