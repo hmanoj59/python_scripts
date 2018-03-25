dca67430d11cdbc788861cce46bfdcfcb43e899b
Naga Hota <hotasmail@gmail.com>
2018-03-25 02:04:48 -0400

    refactored code

--- string_reversal.py
+++ string_reversal.py
@@ -1,32 +1,11 @@
-def rev_word3(s):
-    """
-    Manually doing the splits on the spaces.
-    """
-
-    words = []
-    length = len(s)
-    spaces = [' ']
-
-    # Index Tracker
+def ref_str(str):
+    output = []
     i = 0
-
-    # While index is less than length of string
-    while i < length:
-
-        # If element isn't a space
-        if s[i] not in spaces:
-
-            # The word starts at this index
-            word_start = i
-
-            while i < length and s[i] not in spaces:
-
-                # Get index where word ends
+    while i < len(str):
+        if str[i] != ' ':
+            first_char = i
+            while i < len(str) and str[i] != ' ':
                 i += 1
-            # Append that word to the list
-            words.append(s[word_start:i])
-        # Add to index
-        i += 1
-
-    # Join the reversed words
-    return " ".join(reversed(words))
+            output.append(str[first_char:i])
+        i +=1
+    return " ".join(reversed(output))
