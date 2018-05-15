71a9d3c460787e04662161d3801aa855b7fe7a8e
Naga Hota <hotasmail@gmail.com>
2018-05-15 01:04:52 -0400

    large continous sum

--- practise1.py
+++ practise1.py
@@ -105,4 +105,13 @@ def uniq(s):
         else:
             output.add(ele)
     return True
-uniq('abcdefg')    
+uniq('abcdefg')
+def large_cont_sum(arr):
+    if len(arr) == 0:
+        return 0
+    current_sum = max_sum = arr[0]
+    for ele in arr[1:]:
+        current_sum = max(current_sum+ele,ele)
+        max_sum = max(current_sum, max_sum)
+    return max_sum
+large_cont_sum([1,2,-1,3,4,10,10,-10,-1])    
